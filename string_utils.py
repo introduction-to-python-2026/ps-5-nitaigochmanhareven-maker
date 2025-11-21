c94822776334126f0acd4e2bca775387b7a5c450
nitaigochmanhareven-maker <nitai.gochman.hareven@gmail.com>
2025-11-21 15:27:00 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -11,14 +11,20 @@ def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
     Example: 'H2O' → {'H': 2, 'O': 1}"""
 
-    # Step 1: Initialize an empty dictionary to store atom counts
+    {} = atom_counts
+    split_by_capitals(molecular-formula):
+        atom_name ,atom_count = split_at_number(atom)
 
     for atom in split_by_capitals(molecular_formula):
         atom_name, atom_count = split_at_number(atom)
         
-        # Step 2: Update the dictionary with the atom name and count
+        if atom_name in atom_counts:
+            atom_counts[atom_name] += atom_count
+        else:
+            atom_counts[atom_name] = atom_count
+        
 
-    # Step 3: Return the completed dictionary
+    return atom_counts
 
 
 
